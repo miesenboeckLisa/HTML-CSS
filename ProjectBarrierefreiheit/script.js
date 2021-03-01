83c42692259368447b26013dbc5ff2b6d989c25f
LisaMie <79650760+miesenboeckLisa@users.noreply.github.com>
2021-03-01 11:14:27 +0100

    Delete ProjectBarrierefreiheit directory

--- ProjectBarrierefreiheit/script.js
+++
@@ -1,9 +0,0 @@
-//Mit JQuery
-$(document).ready(function () {
-    $( "#skip_menu" ).focusin(function() {
-        $(this).removeClass('hide');
-    });
-    $( "#skip_menu" ).focusout(function() {
-        $(this).addClass('hide');
-    });
-});
